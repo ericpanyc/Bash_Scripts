627c69a6754a31ab443fc6a563cab0e7a15519df
ericpanyc <yachenpan1005@gmail.com>
2019-09-18 11:19:18 -0700

    1.Update README\n2.Automatically delete files generated in the last run\n3.Add some default value and conditions to avoid traceback by missing options\n4.Change the option name "keyword" to "term"

--- generate_cloud.sh
+++ generate_cloud.sh
@@ -1,21 +1,24 @@
 #!/usr/bin/bash
-help_message="------------------[OPTIONS]--------------------\n\n--keyword (text)--------------the topic of the wordcloud\n--background (color)----------specify a color for background\n--mask (file)-----------------mask to use for the image form\n--color (color)---------------specify a color for image\n--name (text)-----------------specify the name of the image"
+help_message="------------------[OPTIONS]--------------------\n\n--keyword (text)--------------the topic of the wordcloud\n--background (color)----------specify a color for background\n--mask (file)-----------------mask to use for the image form\n--color (color)---------------specify a color for image\n--name (text)-----------------specify the name of the image\n\nPlease use double quote \"\" to include all your terms you want to look up, and delimit each of them by a space.\n\nBased on the terms you input, it might take a rather long time to fetch data from the NCBI database and then generate the word cloud, so, please have a cup of coffee and be patient."
+
 if [ -z "$1" ]; then echo -e $help_message
 else
-    OPTS=`getopt -o z --long keyword:,background:,mask:,color:,name: -n 'parse-options' -- "$@"`
+    if [ -f pubmed.txt ]; then rm pubmed.txt; fi
+    find . -name "*.png" -type f|xargs rm -f
+    
+    OPTS=`getopt -o z --long term:,background:,mask:,color:,name: -n 'parse-options' -- "$@"`
     if [ $? != 0 ] ; then echo "Failed parsing options." >&2 ; exit 1 ; fi
-
     eval set -- "$OPTS"
 
-    KEYWORD=''
-    BACKGROUND=''
+    TERM=''
+    BACKGROUND='black'
     MASK=''
-    COLOR=''
-    NAME=''
+    COLOR='red'
+    NAME='wordcloud'
 
     while true; do
         case "$1" in
-            --keyword ) KEYWORD="$2"; shift; shift ;;
+            --term ) TERM="$2"; shift; shift ;;
             --background ) BACKGROUND="$2"; shift; shift ;;
             --mask ) MASK="$2"; shift; shift ;;
             --color ) COLOR="$2"; shift; shift ;;
@@ -24,16 +27,13 @@ else
         esac
     done
 
-    esearch -db pubmed -query "$KEYWORD" | efetch -format abstract > pubmed.txt
-    wordcloud_cli --text pubmed.txt --imagefile $NAME.png --color $COLOR --background $BACKGROUND --mask ~/Bash_Scripts/$MASK.jpg
-
-
-
-
-
-
-
-
+    esearch -db pubmed -query "$TERM" | efetch -format abstract > pubmed.txt
+    if  [ -z "$MASK" ]
+    then
+        wordcloud_cli --text pubmed.txt --imagefile $NAME.png --color $COLOR --background $BACKGROUND
+    else
+        wordcloud_cli --text pubmed.txt --imagefile $NAME.png --color $COLOR --background $BACKGROUND --mask $MASK.jpg
+    fi
 fi
 
 
